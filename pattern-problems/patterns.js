5118d6a8f4c743b2d279732c6571e8b4cf17b5ea
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-14 19:00:42 +0530

    add pyramid problem

--- pattern-problems/patterns.js
+++ pattern-problems/patterns.js
@@ -53,3 +53,80 @@ const triangleNumPattern = (n) => {
 };
 
 triangleNumPattern(10);
+
+// Given an integer N, print the following pattern :
+// Input Format: N = 3
+// Result:
+// 1
+// 2 2
+// 3 3 3
+
+const pattern = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+      row += i + 1;
+    }
+    console.log(row);
+  }
+};
+
+pattern(5);
+
+//Given an integer N, print the following pattern :
+// Input Format: N = 3
+// Result:
+// * * *
+// * *
+// *
+
+const reverseTrianglePattern = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i; j++) {
+      //reduce the loop of j by n-i
+      row += "* ";
+    }
+    console.log(row);
+  }
+};
+
+reverseTrianglePattern(5);
+
+// Given an integer N, print the following pattern :
+// Input Format: N = 3
+// Result:
+// 1 2 3
+// 1 2
+// 1
+const reverseNumTriangle = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i; j++) {
+      row += j + 1;
+    }
+    console.log(row);
+  }
+};
+
+reverseNumTriangle(5);
+
+const pyramidStart = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i - 1; j++) {
+      row += " ";
+    }
+
+    for (let j = 0; j < 2 * i + 1; j++) {
+      row += "*";
+    }
+
+    for (let j = 0; j < n - i - 1; j++) {
+      row += " ";
+    }
+    console.log(row);
+  }
+};
+
+pyramidStart(6);
