afbb608f3a0b23e1f5121ff10290c291f2800631
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-20 13:07:59 +0530

    add pattern

--- pattern-problems/patterns.js
+++ pattern-problems/patterns.js
@@ -207,3 +207,93 @@ const pattern11 = (n) => {
 };
 
 pattern11(5);
+
+// 1        1
+// 12      21
+// 12     321
+// 1234  4321
+// 1234554321
+
+const pattern12 = (n) => {
+  for (let row = 0; row < n; row++) {
+    let num = "";
+    for (let col = 0; col < row; col++) {
+      num += col + 1;
+    }
+    for (let col = 0; col < 2 * (n - row - 1); col++) {
+      num += " ";
+    }
+    for (let col = 0; col < row; col++) {
+      num += row - col;
+    }
+    console.log(num);
+  }
+};
+
+pattern12(5);
+
+// 1
+// 2  3
+// 4  5  6
+// 7  8  9  10
+// 11 12 13 14  15
+
+const pattern13 = (n) => {
+  let num = 1;
+  for (let row = 0; row < n; row++) {
+    let s = "";
+    for (let col = 0; col < row; col++) {
+      s += num + " ";
+      num++;
+    }
+    console.log(num);
+  }
+};
+
+pattern13(5);
+
+const pattern14 = (n) => {
+  for (let row = 1; row <= n; row++) {
+    let star = "";
+    for (let col = 1; col <= n; col++) {
+      if (row == 1 || col == 1 || row == n || col == n) {
+        star += "*";
+      } else {
+        star += " ";
+      }
+    }
+    console.log(star);
+  }
+};
+
+pattern14(5);
+
+const pattern15 = (n) => {
+  for (let row = 0; row < n; row++) {
+    let char = "";
+    for (let col = "A"; col <= "A" + row; col++) {
+      char += col;
+    }
+    console.log(char);
+  }
+};
+
+pattern15(5);
+
+const pattern16 = (n) => {
+  for (let row = 1; row <= n; row++) {
+    let star = "";
+    for (let col = 0; col <= n; col++) {
+      star += "*";
+    }
+    for (let col = 0; col <= 2 * (n - row); col++) {
+      star += " ";
+    }
+    for (let col = 0; col <= n; col++) {
+      star += "*";
+    }
+    console.log(star);
+  }
+};
+
+pattern16(5);
